81e802de314681bdcb08be31a767f8ae37167f49
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2024-10-24 14:40:02 +0200

    Add files via upload

---
+++ Hito-1-1T-PSP-main/Hito-1-1T-PSP-main/java/actividad1/src/programacionmultitarea/Examinador.java
@@ -0,0 +1,29 @@
+package programacionmultitarea;
+
+import java.util.Random;
+
+public class Examinador implements Runnable {
+    private Thread hilo;
+    private BufferExamenes buffer;
+
+    public Examinador(String alumno, BufferExamenes generador) {
+        this.buffer = generador;
+        hilo = new Thread(this, alumno);
+        hilo.start();
+    }
+
+    @Override
+    public void run() {
+        String codigoExamen = this.buffer.consumirExamen();
+        if (codigoExamen != null) {
+            Random rand = new Random();
+            for (int i = 1; i <= 10; i++) {
+                char respuesta = (char) ('A' + rand.nextInt(5));
+                if (respuesta > 'D') respuesta = '-';
+                System.out.println(codigoExamen + ";" + hilo.getName() + "; Pregunta " + i + ";" + respuesta);
+            }
+        } else {
+            System.out.println("Agotado tiempo de espera y no hay más exámenes");
+        }
+    }
+}
